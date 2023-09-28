7a7af995f021bf6e2f79c1e61f5ab2cd86677ccc
Loïc Prosi <loicprosi@gmail.com>
2023-09-28 18:50:37 +0200

    feat: added server and app logic

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+import cors from "./middlewares/cors.js";
+
+import bookRoutes from "./routes/book.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_TEST_USERNAME}:${process.env.DB_TEST_PASSWORD}@test.euxswqz.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("MongoDB connexion OK"))
+  .catch((error) => console.log("MongoDB connection error: ", error));
+
+app.use(express.json());
+app.use(cors);
+// Serve images folder to client
+app.use("/images", express.static(new URL("images", import.meta.url).pathname));
+
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", authRoutes);
+
+export default app;
